adf3a0bdb5c78ba67ad9d1b8d6edc86a884e34dc
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-04-02 13:13:50 -0400

    update index.php
    fake api url

--- index.php
+++ index.php
@@ -10,22 +10,9 @@
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->headers->set('Access-Control-Allow-Origin', '*');
 
-    $app->get('/faketoken', function () {
-        $token = new stdClass();
-        $token->token = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 16);
-        $token->expires_at = time() - (rand(1,5)) + 60;
-        echo json_encode($token);
-    });
-
-    $app->get('/token', function () {
-        $client = new HttpClient();
-        $response = $client->get('http://api-dev.theorchard.io/auth/session/?token=d354fb818a642012c1f6189141f1ac9f&client=5656789871&user=8736')->send();
-        echo $response->getBody();        
-    });
-
     $app->get('/analytics/all/', function () {
         $client = new HttpClient();
-        $response = $client->get('http://api-dev.theorchard.io/analytics/all/?' . $_SERVER['QUERY_STRING'])->send();
+        $response = $client->get('http://fake-api.com/?' . $_SERVER['QUERY_STRING'])->send();
         echo $response->getBody();
     });
 
