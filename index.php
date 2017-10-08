f035dc502cd2b6ac3f4c7d19265bc89745bd00ea
Alexey Pavlov <mail@pavalx.ru>
2017-10-08 23:55:35 +0300

    Обновлено до 5.6 синтаксиса

--- index.php
+++ index.php
@@ -4,18 +4,16 @@
     use Guzzle\Http\Client as HttpClient;
     use Slim\Slim;
 
-    $app = new Slim(array(
+    $app = new Slim([
         'debug' => true
-    ));
+    ]);
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->headers->set('Access-Control-Allow-Origin', '*');
 
-    $app->get('/analytics/all/', function () {
+    $app->get('/analytics/all/', function () use($app) {
         $client = new HttpClient();
-        $response = $client->get('http://fake-api.com/?' . $_SERVER['QUERY_STRING'])->send();
+        $response = $client->get('http://fake-api.com/?' . $app->request->getQuery())->send();
         echo $response->getBody();
     });
 
     $app->run();
-    
-?>
